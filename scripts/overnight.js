0e3abf53a7050e26aee13daf5b5b3d445497cd46
aloro lee <alorotams@gmail.com>
2024-10-18 14:53:28 -0400

    script update

--- scripts/overnight.js
+++ scripts/overnight.js
@@ -1,5 +1,21 @@
 
+function getReceipt (checkInDate, roomType, wantsQueen, wantsKing, wantsSuite, numberOfStay){
+    let (checkInDate >= 6 && ) 
+}
+
 document.addEventListener("DOMContentLoaded", ()=>{
+    const firstname = document.getElementById("firstname");
+    const lastname = document.getElementById("lastname");
+    const email = document.getElementById("email");
+    const nights = document.getElementById("nights");
+    const queen = document.getElementById("queen");
+    const king = document.getElementById("king");
+    const suite = document.getElementById("suite");
+    const adults = document.getElementById("adults");
+    const children = document.getElementById("children");
+    const none = document.getElementById("none");
+    const aaa = document.getElementById("aaa");
+    const military = document.getElementById("military");
 
     const overnightForm = document.getElementById("overnightForm");
     const when = document.getElementById("when");
@@ -9,6 +25,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
     
     overnightForm.addEventListener("submit", (event)=>{
         event.preventDefault();
-        output.innerText = when.value;
+        output.innerText = getReceipt(
+            when.value,
+            nights.value,
+            queen.checked,
+            king.checked,
+            suite.checked,
+            adults.value,
+            children.value,
+            none.checked,
+            aaa.checked,
+            military.checked
+        );
     });
 });
