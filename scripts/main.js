52ab4fe5400d55214d37dcfc1cbfce4b69c46e03
aloro lee <alorotams@gmail.com>
2024-10-18 10:59:11 -0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const header = `
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="diningservice.html">Dining</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="overnight.html">Overnight</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="rentalcar.html">Car Rental</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="activities.html">Activities</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+`
