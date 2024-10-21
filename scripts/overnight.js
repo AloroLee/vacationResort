5063e732bfdc5a02a4a5faeba16a2776cb106a72
aloro lee <alorotams@gmail.com>
2024-10-20 23:15:09 -0400

    Progress on Overnight JS

--- scripts/overnight.js
+++ scripts/overnight.js
@@ -1,90 +1,73 @@
 
-function getReceipt (checkInDate, roomType, wantsQueen, wantsKing, wantsSuite, numberOfStay){
+function getReceipt (checkInDate, roomType, wantsAAA, wantsMilitary, nights){
     const date = new Date(checkInDate)
     const month = date.getMonth();
-    console.log("month()",month)
     const isPEAK = month >=5 && month <=7;
-    let rate;
+    let roomRate = 0;
     if (isPEAK) {
-        if("queen" === roomType)
-            roomRate = 250;
-    }else {
-        if("queen" === roomType)
-            roomRate = 150;
+        if("queen" === roomType) roomRate = 250;
+        if("king" === roomType) roomRate = 250;
+        if("suite" === roomType) roomRate = 350;
+    } else {
+        if("queen" === roomType) roomRate = 150;
+        if("king" === roomType) roomRate = 150;
+        if("suite" === roomType) roomRate = 210;
     }
-    if (isPEAK) {
-        if("king" === roomType)
-            roomRate = 250;
-    }else {
-        if("king" === roomType)
-            roomRate = 150;
-    }
-    if (isPEAK) {
-        if("suite" === roomType)
-            roomRate = 350;
-    }else {
-        if("suite" === roomType)
-            roomRate = 210;
-    }
-    return roomRate;
+
+    let totalRoomCost = roomRate * nights;
 
     let discount = 0;
-    if (discount === aaa){
+    if (wantsAAA){
         discount = 0.10;
     }else
-        if(discount === military){
+        if(wantsMilitary){
         discount = 0.20;
     }
-    const discountRoomCost = totalRoomCost * (1 - discount);
+    let discountRoomCost = totalRoomCost * (1 - discount);
     
-    const tax = discountRoomCost * 0.12;
-    let totalRoomCost = roomRate * nights;
-    const totalTax = tax * nights;
-    const totalCost = totalRoomCost + totalTax;
-    output.innerText = `
+    let tax = discountRoomCost * 0.12;
+    let totalTax = tax * nights;
+    let totalCost = totalRoomCost + totalTax;
+    
+    return `
         Room Cost: $${roomRate.toFixed(2)}
         Discount Rate: ${discount}
-        Tax: $${tax}
+        Tax: $${tax.toFixed(2)}
         Total Room Cost: $${totalCost.toFixed(2)}
     `;
-
-
-
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    const firstname = document.getElementById("firstname");
-    const lastname = document.getElementById("lastname");
-    const email = document.getElementById("email");
-    const nights = Number(document.getElementById("nights").value);
     const queen = document.getElementById("queen");
     const king = document.getElementById("king");
     const suite = document.getElementById("suite");
-    const adults = document.getElementById("adults");
-    const children = document.getElementById("children");
-    const none = document.getElementById("none");
     const aaa = document.getElementById("aaa");
     const military = document.getElementById("military");
 
     const overnightForm = document.getElementById("overnightForm");
     const when = document.getElementById("when");
-    const output = document.getElementById("output");
+    const output = document.getElementById("receiptOutput");
 
 
     
     overnightForm.addEventListener("submit", (event)=>{
         event.preventDefault();
+
+        const nights = Number(document.getElementById("nights").value);
+
+
+        let roomType = "queen";  // default room type
+        if (queen.checked) roomType = "queen";
+        else if (king.checked) roomType = "king";
+        else if (suite.checked) roomType = "suite";
+
         output.innerText = getReceipt(
             when.value,
-            nights.value,
-            queen.checked,
-            king.checked,
-            suite.checked,
-            adults.value,
-            children.value,
-            none.checked,
+            roomType,
             aaa.checked,
-            military.checked
+            military.checked,
+            nights
+
         );
     });
 });
